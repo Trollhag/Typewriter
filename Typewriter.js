51521af3d3acb77754b4cd9f1fc7685f2fef27fd
Trollhag <oscar.trollhag@gmail.com>
2016-04-21 18:13:10 +0200

    v1.2
    - Added support for  custom backspaceDelay on instance level
    - Added support for tracing to other instance ["new", 0] = ["new","new"]
    - Added some TODO comments as reminders
    - Generell clean up and commenting

--- Typewriter.js
+++ Typewriter.js
@@ -1,20 +1,27 @@
 function Typewriter(args) {
-    this.str = args.type.slice();
-    this.origin = args.type.slice();
-    this.marker = $('<span data-marker="">|</span>').appendTo(args.target);
-    this.delay = args.delay;
-    this.removeDelay = args.removeDelay;
+    // TODO: Change str/Type wait/backspace syntax to array rather than clumsy string styntax, easier to read, easier to parse.
+    this.str = args.Type.slice();
+    this.origin = args.Type.slice();
+    this.marker = $('<span class="typewriter-marker">|</span>').appendTo(args.Target);
+    this.delay = args.Delay;
+    this.removeDelay = args.BackspaceDelay;
     this.Start();
 }
 Typewriter.prototype.Start = function() {
     var that = this;
-    this.Repeat();
-    if (this.str[0].match(/[\[\]]|(?!\w*\:\d+)\|.*/g)) {
-        var operators = this.str.shift().replace(/[\[\]]/g, '').split('|');
-        operators.forEach(function(v) {
-        });
+    // Makes it possible to backtrack to previous instance, looks better than C/P code.
+    if (Number.isInteger(this.str[0]) && !Number.isInteger(this.origin[this.str[0]]) && this.origin[this.str[0]] != undefined) {
+        this.instance = this.origin[this.str.shift()];
+    }
+    else {
+        this.Repeat();
+        this.instance = this.str.shift();
+    }
+    if (this.instance.match(/[\[\]]|(?!\w*\:\d+)\|.*/g)) {
+        var operators = this.instance.replace(/[\[\]]/g, '').split('|');
         var c = 0;
         loop();
+        
         function loop() {
             if (operators[c].indexOf("wait") > -1) {
                 setTimeout(function(){
@@ -29,7 +36,23 @@ Typewriter.prototype.Start = function() {
                 }, parseInt(operators[c].replace(/[^\d]/g, '')));
             }
             else if (operators[c].indexOf('backspace') > -1) {
-                that.Backspace(parseInt(operators[c].replace(/[^\d]/g, '')), function() {
+                // TODO: Move this junk to Backspace prototype function, it does not belong here.
+                var n = 0;
+                var d = null;
+                var arr = operators[c].split(':');
+                
+                if (arr[1].toLowerCase() === "all" || parseInt(arr[1]) === 0) {
+                    n = that.marker.siblings().length;
+                }
+                else {
+                    n = parseInt(arr[1].replace(/[^\d]/g, ''));
+                }
+                
+                if (operators[c].match(/backspace\:.*\:\d*/)) {
+                    d = parseInt(arr[2]);
+                }
+                
+                that.Backspace(n, d, function() {
                     if (c == operators.length-1) {
                         return true;
                     }
@@ -43,10 +66,14 @@ Typewriter.prototype.Start = function() {
         }
     }
     else {
-        this.OutPut(this.str.shift());
+        this.OutPut(this.instance);
     }
 }
 Typewriter.prototype.OutPut = function(letters) {
+    /* TODO:
+        - Add option to change delay/type-speed on instance-level
+        - Add algorithm for delay/type-speed (eg. easeing, swing, ...)
+    */
     var that = this;
     var l = letters.split('');
     var c = 0;
@@ -62,7 +89,7 @@ Typewriter.prototype.OutPut = function(letters) {
         }, that.delay || 800);
     }
 }
-Typewriter.prototype.Backspace = function(n, complete) {
+Typewriter.prototype.Backspace = function(n, d, complete) {
     var that = this;
     if(!$.isFunction(complete)) complete = function() {};
     var c = 1;
@@ -79,7 +106,7 @@ Typewriter.prototype.Backspace = function(n, complete) {
                 c++;
                 Remove();
             }
-        }, that.removeDelay || that.delay || 400);
+        }, d || that.removeDelay || that.delay || 400);
     }
 }
 Typewriter.prototype.Repeat = function() {
